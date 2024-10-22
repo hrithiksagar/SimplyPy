47d4c6ebd008cda86da6b60b7eb52260ddd00866
Hrithik Sagar <hrithiksagar36@gmail.com>
2024-10-22 18:08:29 +0530

    Update

--- setup.py
+++ setup.py
@@ -1,1 +1,14 @@
 
+from setuptools import find_packages, setup
+
+setup(
+    name='simplypy',
+    packages=find_packages(include=['simplypy']),
+    version='0.1.0',
+    description='My first Python library',
+    author='Hrithik_Sagar',
+    install_requires=[],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest==4.4.1'],
+    test_suite='tests',
+)
